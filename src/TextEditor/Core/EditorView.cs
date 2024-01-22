5688c21546d0cea450208dacd2c79e57c9d2f231
wiechecdawid <wiechec.dawid@gmail.com>
2024-01-23 00:53:26 +0100

    get buffered lines added

--- src/TextEditor/Core/EditorView.cs
+++ src/TextEditor/Core/EditorView.cs
@@ -3,13 +3,12 @@ namespace TextEditor.Core;
 internal class EditorView
 {
     private readonly FileHandler _fileHandler;
-    private readonly ConsoleHandler _consoleHandler;
     private string _buffer;
-}
 
-internal class ConsoleHandler
-{
-
+    public EditorView(FileHandler fh, int bufferSize)
+    {
+        
+    }
 }
 
 internal class FileHandler
@@ -18,5 +17,22 @@ internal class FileHandler
     public FileHandler(string path)
     {
         _fs = File.Open(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    }
+
+    public string[] GetBufferLines(int start, int lines)
+    {
+        var buffer = new List<string>();
+        using var sr = new StreamReader(_fs);
+        int index = 0;
+        while (sr.Peek() >= 0 || index < start + lines) {
+            var line = sr.ReadLine();
+            if (index >= start) 
+            {
+                buffer.Add(line);
+            }
+            index ++;
+        }
+
+        return [.. buffer];
     }
 }
