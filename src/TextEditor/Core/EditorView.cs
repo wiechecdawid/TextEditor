fdaeaa0490cbe65127725d2575923f45866d8f91
wiechecdawid <wiechec.dawid@gmail.com>
2024-02-02 02:03:24 +0100

    jfuyrfjh

--- src/TextEditor/Core/EditorView.cs
+++ src/TextEditor/Core/EditorView.cs
@@ -1,13 +1,76 @@
 namespace TextEditor.Core;
 
+internal class ConsoleHandler
+{
+    public void Print(string buffer)
+    {
+        Console.Clear();
+        Console.WriteLine(buffer);
+    }
+}
+
 internal class EditorView
 {
+    private readonly Buffer _buffer;
     private readonly FileHandler _fileHandler;
-    private string _buffer;
+    private readonly ConsoleHandler _consoleHandler;
 
-    public EditorView(FileHandler fh, int bufferSize)
+    public EditorView(Buffer buffer, ConsoleHandler ch, FileHandler fh)
     {
-        
+        _consoleHandler = ch;
+        _buffer = buffer;
+        _fileHandler = fh;
+    }
+
+    public void DisplayBuffer()
+    {
+        _consoleHandler.Print(_buffer.GetBuffer());
+    }
+
+    public void OnKeyPressed(ConsoleKeyInfo key)
+    {
+        key.Key switch
+        {
+            ConsoleKey.UpArrow => _consoleHandler.Print("up"),
+        };
+    }
+}
+
+internal class Buffer
+{
+    private int _size;
+    private string[] _buffer;
+    private Action<IEnumerable<string>> _onBufferChanged;
+
+    public void Subscribe(Action<IEnumerable<string>> action)
+    {
+        _onBufferChanged += action;
+    }
+
+    public string GetBuffer()
+    {
+        return string.Join('\n', _buffer);
+    }
+
+    public void Unsubscribe(Action<IEnumerable<string>> action)
+    {
+        if (_onBufferChanged is not null)
+        {
+            _onBufferChanged -= action;
+        }
+    }
+
+    public void WriteToBuffer(int l, int col, ReadOnlySpan<string> txt)
+    {
+        // ref string line = ref _buffer[l];
+        // line.Insert(col, txt.ToString());
+        // _onBufferChanged?.Invoke(_buffer);
+        throw new NotImplementedException();
+    }
+
+    public void RemoveFromBuffer(int l, int col, int count)
+    {
+        throw new NotImplementedException();
     }
 }
 
@@ -19,7 +82,7 @@ internal class FileHandler
         _fs = File.Open(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
     }
 
-    public string[] GetBufferLines(int start, int lines)
+    public string[] StreamLines(int start, int lines)
     {
         var buffer = new List<string>();
         using var sr = new StreamReader(_fs);
--- src/TextEditor/Program.cs
+++ src/TextEditor/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var name = args[0];
 Console.WriteLine($"Hello, {name}!");
+
+
