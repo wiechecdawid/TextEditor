a98689382277eca4092119d9395068612b66b30c
wiechecdawid <wiechec.dawid@gmail.com>
2024-02-20 01:46:13 +0100

    keymap

--- src/TextEditor/Core/ConsoleHandler.cs
+++ src/TextEditor/Core/ConsoleHandler.cs
@@ -1,34 +1,41 @@
 namespace TextEditor.Core;
 
+internal record struct Cursor(int Top, int Left);
+
 internal class ConsoleHandler
 {
     public event EventHandler<ConsoleResizedEventArgs>? OnConsoleResized;
-    private int _height;
-    private int _width;
     private readonly CancellationTokenSource _cts;
     private readonly KeyMap _map;
+    private Cursor _cursor;
 
     public ConsoleHandler(CancellationTokenSource cts)
     {
-        _height = Console.WindowHeight;
-        _width = Console.WindowWidth;
+        _cursor = new Cursor(Console.WindowHeight, Console.WindowWidth);
         _cts = cts;
         _map = new KeyMap();
     }
 
     public (int, int) GetSizes()
     {
-        return (_height, _width);
+        return (_cursor.Top, _cursor.Left);
     }
 
     public void UpdateSizes()
     {
-        if (_height != Console.WindowHeight || _width != Console.WindowWidth)
+        if (_cursor.Top != Console.WindowHeight || _cursor.Left != Console.WindowWidth)
         {
-            _height = Console.WindowHeight;
-            _width = Console.WindowWidth;
-            OnConsoleResized?.Invoke(this, new ConsoleResizedEventArgs(_height, _width));
+            _cursor.Top = Console.WindowHeight;
+            _cursor.Left = Console.WindowWidth;
+            OnConsoleResized?.Invoke(this, new ConsoleResizedEventArgs(_cursor.Top, _cursor.Left));
         }
+    }
+
+    public void ChangeCursorPosition(int top, int left)
+    {
+        Console.SetCursorPosition(left, top);
+        _cursor.Top = top;
+        _cursor.Left = left;
     }
 
     public void ReadKey()
@@ -53,5 +60,53 @@ internal class ConsoleHandler
     {
         Console.Clear();
         Console.Write(string.Join('\n', buffer));
+    }
+}
+
+
+internal class CursorMover(ConsoleHandler Handler)
+{
+    public void MoveRight()
+    {
+        var (top, left) = Handler.GetSizes();
+        if (left < Console.WindowWidth - 1)
+        {
+            Handler.ChangeCursorPosition(top, left + 1);
+        }
+        else
+        {
+            Handler.ChangeCursorPosition(top + 1, 0);
+        }
+    }
+
+    public void MoveLeft()
+    {
+        var (top, left) = Handler.GetSizes();
+        if (left > 0)
+        {
+            Handler.ChangeCursorPosition(top, left - 1);
+        }
+        else
+        {
+            Handler.ChangeCursorPosition(top - 1, Console.WindowWidth - 1);
+        }
+    }
+
+    public void MoveUp()
+    {
+        var (top, left) = Handler.GetSizes();
+        if (top > 0)
+        {
+            Handler.ChangeCursorPosition(top - 1, left);
+        }
+    }
+
+    public void MoveDown()
+    {
+        var (top, left) = Handler.GetSizes();
+        if (top < Console.WindowHeight - 1)
+        {
+            Handler.ChangeCursorPosition(top + 1, left);
+        }
     }
 }
